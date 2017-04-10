d0544f9476c656dbad098d01587d902eacabf69e
chaR94 <charlotte.rambourg@insa-cvl.fr>
2017-04-10 20:05:39 +0200

    Suite

--- src/example/Hello.java
+++ src/example/Hello.java
@@ -2,6 +2,12 @@
 package example;
 
 import org.apache.commons.lang.WordUtils;
+import org.apache.commons.lang.WordUtils;
+import com.opencsv.*;
+import java.util.List;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Hello {
     public static void main(String[] args) {
@@ -9,6 +15,21 @@ public class Hello {
         System.out.println("standard message : " + message);
         System.out.println("capitalized by " + WordUtils.class.getName() 
                                 + " : " + WordUtils.capitalizeFully(message));
+
+        int monmax=0;
+        CSVReader reader = new CSVREADER(new FileReader("data.csv"));
+        List<String[]> myEntries=reader.readAll();
+
+        for(String[] line: myEntries){
+        	for(String val: line){
+        		
+        		if (Integer.parseInt(val) > monmax){
+        			monmax = Integer.parseInt(val);
+        		}
+        	}
+        }
+        System.println("Le Max est:"+monmax);
+
     }
 }
         
